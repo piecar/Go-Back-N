f9f8d6f68bd9ef41b05874e63994f896b522df1c
Pierre Jimenez <pjime032@fiu.edu>
2015-11-13 20:25:58 -0500

    Building of header complete

--- sender.c
+++ sender.c
@@ -7,29 +7,34 @@
 #include <netinet/in.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 #define BUFFSIZE 256
 #define MAXPAY 1024
-#define PACKSIZE 32 // 1B ACK, 6B seq#, 6B numPackets, 2B checksum, 3B for spaces
+#define HSIZE 15 // 1B ACK, 4B seq#, 4B numPackets, 2B checksum, 4B for spaces
 #define WINSIZE 100
 
 void syserr(char *msg) { perror(msg); exit(-1); }
 void ftpcomm(int newsockfd, char* buffer);
 void readandsend(int tempfd, int newsockfd, char* buffer);
 void recvandwrite(int tempfd, int newsockfd, int size, char* buffer);
+uint16_t ChkSum(char * packet);
 
 int main(int argc, char *argv[])
 {
-  int sockfd, newsockfd, portno, pid, size;
-  uint32_t seqNum;
-  short checksum;
-  struct sockaddr_in serv_addr, clt_addr;  
+  int sockfd, newsockfd, tempfd, portno, pid, size;
+  uint32_t seqNum, numPackets;
+  uint16_t checksum;
+  uint8_t ack;
+  struct sockaddr_in serv_addr, clt_addr; 
+  struct hostent* server; 
   struct stat filestats;
   socklen_t addrlen;
   char * recvIP;
   char buffer[BUFFSIZE];
   char payload[MAXPAY];
 
-  if(argc != 3) { 
+  if(argc != 4) { 
     fprintf(stderr,"Usage: %s <IP> <port> <name of file>\n", argv[0]);
     return 1;
   } 
@@ -37,65 +42,85 @@ int main(int argc, char *argv[])
   { 
 	server = gethostbyname(argv[1]);
 	if(!server) {
-	fprintf(stderr, "ERROR: no such host: %s\n", argv[1]);
-	return 2;
+		fprintf(stderr, "ERROR: no such host: %s\n", argv[1]);
+		return 2;
 	}
   	portno = atoi(argv[2]);
   }
   
   //Socket Logic
-  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
   if(sockfd < 0) syserr("can't open socket"); 
   	printf("create socket...\n");
 
   memset(&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_addr = *((struct in_addr*)server->h_addr);
   serv_addr.sin_port = htons(portno);
-
-  if(bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) 
-    syserr("can't bind");
-  printf("bind socket to port %d...\n", portno);
   
   //Make array of packet elements to send
-  stat(arvg[3], &filestats);
+  stat(argv[3], &filestats);
   size = filestats.st_size;
-  numPackets = size/PACKSIZE + 1
+  numPackets = size/MAXPAY + 1;
+  printf("num of packets: %d\n", numPackets);
   char * fileArray[numPackets];
   seqNum = 0;
+  checksum = 0;
   //Open File
-  tempfd = open(arvg[3], O_RDWR);
+  tempfd = open(argv[3], O_RDWR);
   if(tempfd < 0) syserr("failed to open file");
   
   //Populate the array with packets to send
   while(1){
   	char * packet;
-  	char str[13] //CHECK THE SIZE
-  	checksum = 0;
-  	packet = malloc(sizeof(char)*(PACKSIZE + MAXPAY));
+  	char str[13]; //CHECK THE SIZE
+  	ack = 0;
+  	packet = malloc(sizeof(char)*(HSIZE + MAXPAY + 1));
   	char payload[MAXPAY];
   	
-  	memset(packet, 0, (PACKSIZE + MAXPAY));
-  	strcpy(packet, "0"); //Set ACK to 0
-  	strcat(packet, " "); //Set space
-  	sprintf(str, "%d", seqNum);
-  	strcat(packet, str); //Set Sequence #
-  	strcat(packet, " "); //Set Space
-  	sprintf(str, "%d", numPackets);
-  	strcat(packet, str); //Set number of packets
-  	strcat(packet, " "); //Set Space
-  	sprintf(str, "%d", checksum);
-  	strcat(packet, str); //Set zero checksum
+  	memset(packet, 0, (HSIZE + MAXPAY));
+  	//set ack
+  	packet[0] = ack & 255;
+  	packet[1] = ' ';
+  	//set seq#
+  	packet[2] = (seqNum >>  24) & 255;
+  	packet[3] = (seqNum >>  16) & 255;
+  	packet[4] = (seqNum >>  8) & 255;
+  	packet[5] = seqNum & 255;
+  	packet[6] = ' ';
+  	//set numPackets
+  	packet[7] = (numPackets >>  24) & 255;
+  	packet[8] = (numPackets >>  16) & 255;
+  	packet[9] = (numPackets >>  8) & 255;
+  	packet[10] = numPackets & 255;
+  	packet[11] = ' ';
+  	//set checksum
+  	packet[12] = (checksum >>  8) & 255;
+  	packet[13] = checksum & 255;
+  	packet[14] = ' ';
+  	
+  	/*
+  	int i;
+  	for(i =0 ; i<16; i++){
+  		printf("packet at %d is: %d\n", i, packet[i]);
+  	}
+  	*/
+  	
   	// Read payload from file, add to packet
   	int bytes_read = read(tempfd, payload, MAXPAY);
-	buffer[bytes_read] = '\0';
+	payload[bytes_read] = '\0';
+  	//printf("bytes read are: %u. packet size is: %u. payload size: %d\n", bytes_read, sizeof(packet), sizeof(payload));
+  	//printf("The string is: %s\n", packet);
 	if (bytes_read == 0) // We're done reading from the file
 		break;
 	if (bytes_read < 0) syserr("error reading file");
 	strcat(packet, payload); //Set Payload
 	
-	char *mkChkSum
-  	
+	checksum = ChkSum(packet);
+  	packet[12] = (checksum >>  8) & 255;
+  	packet[13] = checksum & 255; 
+  	seqNum++; 	
+  	printf("seq num: %d", seqNum);
   }
 
 for(;;) {
@@ -123,6 +148,22 @@ for(;;) {
 unsigned int_to_int(unsigned k) {
     if (k == 0 || k ==1 ) return k;
     return (k % 2) + 10 * int_to_int(k / 2);
+}
+
+uint16_t ChkSum(char * packet){
+	uint16_t checksum = 0, curr = 0, i = 0;
+	int psize = HSIZE + MAXPAY + 1;
+	//sscanf(packet, "%*s %*s %*s %u", &checksum);
+	//printf("checksum is: %d. Packet Size is: %d\n", checksum, psize);
+	while(psize > 0){
+		curr = ((packet[i] << 8) + packet[i+1]) + checksum;
+		checksum = curr + 0x0FFFF;
+		curr = (curr >> 16); //Grab the carryout if it exists
+		checksum = curr + checksum;
+		psize -= 2;
+		i += 2;
+	}
+	return ~checksum;
 }
 
 void ftpcomm(int newsockfd, char* buffer)
